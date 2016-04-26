c320d4ad64e01734c8913d600981c65d5f403cb8
Erin Knapp <erin.knapp@gmail.com>
2016-04-26 01:54:59 -0400

    Updating Ahoy!
    More changes to Lae Dep app and PA2 with mostly working flow

--- LaeDep/app/src/main/java/edu/fau/knapp/group6/login.java
+++ LaeDep/app/src/main/java/edu/fau/knapp/group6/login.java
@@ -5,24 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class login extends AppCompatActivity implements View.OnClickListener {
+public class login extends AppCompatActivity {
 
+    private ImageButton mloginbutton;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        // Assign the views from the layout file to the corresponding variables
-        View activity_main = findViewById(R.id.mainmenu);
-        activity_main.setOnClickListener(this);
 
-    }
+        //View object references
+        mloginbutton = (ImageButton) findViewById(R.id.login_button);
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.mainmenu:
-                Intent j = new Intent(this, MainActivity.class);
-                startActivity(j);
-                break;
-        }
+        mloginbutton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
